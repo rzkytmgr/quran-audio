7e7f7aaeaf79f9a2189421975ff8c1e8d45d63b4
rzkytmgr <rzkytmgr@gmail.com>
2023-04-07 06:25:28 +0700

    add: api routes

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'node:fs';
 import morgan from 'morgan';
 import matter from 'gray-matter';
+import { Routes } from '@/routes';
 import MarkdownIt from 'markdown-it';
 import express, { Request, Response } from 'express';
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use('/static', express.static(path.join(process.cwd(), 'public')));
+app.use('/api', Routes);
 
 // pages routes auto register
 const pages = fs.readdirSync(path.join(process.cwd(), 'pages'));
@@ -33,5 +35,5 @@ app.all('*', (req: Request, res: Response) => {
 });
 
 app.listen(3001, () => {
-  console.log('Running!');
+  console.log('Running! http://localhost:3001');
 });
